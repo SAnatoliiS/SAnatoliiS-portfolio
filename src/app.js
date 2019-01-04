05235e4f5a7834db483014a4ccebd827d3dc614c
Sergeev Anatolii <serg.anatolii.serg@gmail.com>
2019-01-04 09:45:42 +0300

    connect styles and bootstrap

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap';
 import { Provider } from 'react-redux';
 import Main from './containers/Main';
 import configureStore from './store/configureStore';
+import './styles/styles.scss';
 
 const store = configureStore();
 const provider = (
-  <Provider store={store}>
-    <Main />
-  </Provider>
-)
+	<Provider store={store}>
+		<Main />
+	</Provider>
+);
 
 ReactDOM.render(provider, document.getElementById('app'));
